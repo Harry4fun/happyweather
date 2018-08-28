df66b4742edfa176097cfbca2661a86114571197
jkjiao <jkjiao@whchem.com>
2018-08-28 20:29:15 +0800

    3rd
    Signed-off-by: jkjiao <jkjiao@whchem.com>

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,68 @@
+let globalData = getApp().globalData
 Page({
   data: {
+    isIPhoneX: globalData.isIPhoneX,
+    message: '',
+    cityDatas: {},
+    icons: ['/img/clothing.png', '/img/carwashing.png', '/img/pill.png', '/img/running.png', '/img/sun.png'],
+    // 用来清空 input
+    searchText: '',
+    // 是否已经弹出
+    hasPopped: false,
+    animationMain: {},
+    animationOne: {},
+    animationTwo: {},
+    animationThree: {},
+    // 是否切换了城市
+    cityChanged: false,
+    // 需要查询的城市
+    searchCity: '',
+    setting: {},
+    bcgImgList: [
+      {
+        src: '/img/beach-bird-birds-235787.jpg',
+        topColor: '#393836'
+      },
+      {
+        src: '/img/clouds-forest-idyllic-417102.jpg',
+        topColor: '#0085e5'
+      },
+      {
+        src: '/img/backlit-dawn-dusk-327466.jpg',
+        topColor: '#2d2225'
+      },
+      {
+        src: '/img/accomplishment-adventure-clear-sky-585825.jpg',
+        topColor: '#004a89'
+      },
+      {
+        src: '/img/fog-himalayas-landscape-38326.jpg',
+        topColor: '#b8bab9'
+      },
+      {
+        src: '/img/asphalt-blue-sky-clouds-490411.jpg',
+        topColor: '#009ffe'
+      },
+      {
+        src: '/img/aerial-climate-cold-296559.jpg',
+        topColor: '#d6d1e6'
+      },
+      {
+        src: '/img/beautiful-cold-dawn-547115.jpg',
+        topColor: '#ffa5bc'
+      }
+    ],
+    bcgImgIndex: 0,
     bcgImg: '',
-    bcgColor: '#444'
-  }
-})
+    bcgImgAreaShow: false,
+    bcgColor: '#2d2225',
+    // 粗暴直接：移除后再创建，达到初始化组件的作用
+    showHeartbeat: true,
+    // heartbeat 时禁止搜索，防止动画执行
+    enableSearch: true,
+    pos: {},
+    openSettingButtonShow: false
+  
+}
+}
+)

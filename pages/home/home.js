9873d36c9a9a9100ec232a449523059284ff1156
jkjiao <jkjiao@whchem.com>
2018-08-29 18:19:10 +0800

    准备开始实现主页中的各种js方法，包括UI交互与从接口获取数据
    Signed-off-by: jkjiao <jkjiao@whchem.com>

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,84 @@
-let globalData = getApp().globalData
+// pm2.5 浓度对应的指数等级
+// 0-50 优
+// 50-100 良
+// 100-150 轻度污染：对敏感人群不健康
+// 150-200 中度污染：不健康
+// 200-300 重度污染：非常不健康
+// 300-500 严重污染：有毒物
+// 500以上 爆表：有毒物
+let globalData = getApp().globalData;
+let messages = require('../../data/messages.js');
+
 Page({
   data: {
     isIPhoneX: globalData.isIPhoneX,
     message: '',
-    cityDatas: { "originalData": { "error": 0, "status": "success", "date": "2018-08-29", "results": [{ "currentCity": "烟台市", "pm25": "34", "index": [{ "des": "天气热，建议着短裙、短裤、短薄外套、T恤等夏季服装。", "tipt": "穿衣指数", "title": "穿衣", "zs": "热" }, { "des": "较适宜洗车，未来一天无雨，风力较小，擦洗一新的汽车至少能保持一天。", "tipt": "洗车指数", "title": "洗车", "zs": "较适宜" }, { "des": "各项气象条件适宜，无明显降温过程，发生感冒机率较低。", "tipt": "感冒指数", "title": "感冒", "zs": "少发" }, { "des": "天气较好，但因气温较高且风力较强，请适当降低运动强度并注意户外防风。推荐您进行室内运动。", "tipt": "运动指数", "title": "运动", "zs": "较适宜" }, { "des": "属中等强度紫外线辐射天气，外出时建议涂擦SPF高于15、PA+的防晒护肤品，戴帽子、太阳镜。", "tipt": "紫外线强度指数", "title": "紫外线强度", "zs": "中等" }], "weather_data": [{ "date": "周三 08月29日 (实时：26℃)", "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/duoyun.png", "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png", "weather": "多云转晴", "wind": "南风3-4级", "temperature": "28 ~ 22℃" }, { "date": "周四", "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/qing.png", "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png", "weather": "晴", "wind": "东北风3-4级", "temperature": "27 ~ 23℃" }, { "date": "周五", "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/qing.png", "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png", "weather": "晴", "wind": "东北风3-4级", "temperature": "27 ~ 22℃" }, { "date": "周六", "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/duoyun.png", "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png", "weather": "多云转晴", "wind": "东北风3-4级", "temperature": "25 ~ 20℃" }] }] } },
+    cityDatas: {
+      "originalData": {
+        "error": 0,
+        "status": "success",
+        "date": "2018-08-29",
+        "results": [{
+          "currentCity": "烟台市",
+          "pm25": "34",
+          "index": [{
+            "des": "天气热，建议着短裙、短裤、短薄外套、T恤等夏季服装。",
+            "tipt": "穿衣指数",
+            "title": "穿衣",
+            "zs": "热"
+          }, {
+            "des": "较适宜洗车，未来一天无雨，风力较小，擦洗一新的汽车至少能保持一天。",
+            "tipt": "洗车指数",
+            "title": "洗车",
+            "zs": "较适宜"
+          }, {
+            "des": "各项气象条件适宜，无明显降温过程，发生感冒机率较低。",
+            "tipt": "感冒指数",
+            "title": "感冒",
+            "zs": "少发"
+          }, {
+            "des": "天气较好，但因气温较高且风力较强，请适当降低运动强度并注意户外防风。推荐您进行室内运动。",
+            "tipt": "运动指数",
+            "title": "运动",
+            "zs": "较适宜"
+          }, {
+            "des": "属中等强度紫外线辐射天气，外出时建议涂擦SPF高于15、PA+的防晒护肤品，戴帽子、太阳镜。",
+            "tipt": "紫外线强度指数",
+            "title": "紫外线强度",
+            "zs": "中等"
+          }],
+          "weather_data": [{
+            "date": "周三 08月29日 (实时：26℃)",
+            "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/duoyun.png",
+            "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png",
+            "weather": "多云转晴",
+            "wind": "南风3-4级",
+            "temperature": "28 ~ 22℃"
+          }, {
+            "date": "周四",
+            "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/qing.png",
+            "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png",
+            "weather": "晴",
+            "wind": "东北风3-4级",
+            "temperature": "27 ~ 23℃"
+          }, {
+            "date": "周五",
+            "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/qing.png",
+            "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png",
+            "weather": "晴",
+            "wind": "东北风3-4级",
+            "temperature": "27 ~ 22℃"
+          }, {
+            "date": "周六",
+            "dayPictureUrl": "http://api.map.baidu.com/images/weather/day/duoyun.png",
+            "nightPictureUrl": "http://api.map.baidu.com/images/weather/night/qing.png",
+            "weather": "多云转晴",
+            "wind": "东北风3-4级",
+            "temperature": "25 ~ 20℃"
+          }]
+        }]
+      }
+    },
     icons: ['/img/clothing.png', '/img/carwashing.png', '/img/pill.png', '/img/running.png', '/img/sun.png'],
     // 用来清空 input
     searchText: '',
@@ -18,8 +93,7 @@ Page({
     // 需要查询的城市
     searchCity: '',
     setting: {},
-    bcgImgList: [
-      {
+    bcgImgList: [{
         src: '/img/beach-bird-birds-235787.jpg',
         topColor: '#393836'
       },
@@ -62,7 +136,6 @@ Page({
     enableSearch: true,
     pos: {},
     openSettingButtonShow: false
+  },
   
-}
-}
-)
+})
